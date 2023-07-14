444be079ac4c1beeab9782f779be49963a047419
YuTingSun <antia.sun@smartdaily.com.tw>
2023-07-14 13:35:05 +0800

    add fake data

---
+++ WebApplication1/Controllers/CardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        /// <summary>
+        /// 測試用的資料集合
+        /// </summary>
+        private static List<Card> _cards = new List<Card>();
+    }
+}
